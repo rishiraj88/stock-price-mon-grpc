895ce8dee30a176343b34995857791fb052f4c6a
Rishi Raj <rishiraj88+github@gmail.com>
2025-07-07 23:19:25 +0200

    Revisit the project files.

--- stockbose/src/main/java/de/stockbose/StockPriceMonitorApplication.java
+++ stockbose/src/main/java/de/stockbose/StockPriceMonitorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockPriceMonitorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StockPriceMonitorApplication.class, args);
 	}
-
 }
